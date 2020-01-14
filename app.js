7c07fd86c780e98c1a6124ccb98cad127284cb6c
Haim Grosman <haim.grosman@airbnb.com>
2020-01-14 11:23:00 -0800

    playing with html

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ console.log("Hello");
 $(document).ready(function () {
     console.log("Hello");
     now = new Date();
-    $('#js').append($('<p>').append('' + now.toLocaleString()));
+    $('#body').append($('<h1>').append('Hello'));
+    var js = $('#body').append($('<div class="js" id="js">'));
+    js.append($('<p>').append('' + now.toLocaleString()));
 })
